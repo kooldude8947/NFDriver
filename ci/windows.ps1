006bf0225e7c78787fa5a7c7637ffaa89c3b5e6e
kooldude8947 <333993559+kooldude8947@users.noreply.github.com>
2026-09-25 23:19:41 +0000

    Update windows.ps1

--- ci/windows.ps1
+++ ci/windows.ps1
@@ -28,10 +28,10 @@ Write-Host $build
 try
 {
 	# Upgrade pip or else the CI will complain
-	python.exe -m pip install --upgrade pip --upgrade setuptools
+	python.exe -m pip install --upgrade pip
 
 	# Start virtualenv
-	pip install virtualenv
+	pip install virtualenv setuptools
 	virtualenv nfdriver_env
 
 	& ./nfdriver_env/Scripts/activate.bat
